8745015048cf44332370823f21d0f9f3d5ed86da
Marcin Sola <marcinsolaonline@gmail.com>
2021-07-13 21:23:41 +0200

    Compilable contract

---
+++ contracts/StarNotary.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+
+contract StarNotary is ERC721 {
+    struct Star {
+        string name;
+    }
+
+    constructor() ERC721("Start Notary", "STR") {}
+
+    mapping(uint256 => Star) public tokenIdToStarInfo;
+    mapping(uint256 => uint256) public starsForSale;
+
+    function createStar(string memory _name, uint256 _tokenId) public {
+        Star memory newStar = Star(_name);
+        tokenIdToStarInfo[_tokenId] = newStar;
+        _mint(msg.sender, _tokenId);
+    }
+
+    function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
+        require(ownerOf(_tokenId) == msg.sender);
+
+        starsForSale[_tokenId] = _price;
+    }
+
+    function buyStar(uint256 _tokenId) public payable {
+        require(starsForSale[_tokenId] > 0);
+
+        uint256 starCost = starsForSale[_tokenId];
+        address starOwner = ownerOf(_tokenId);
+        require(msg.value >= starCost);
+
+        transferFrom(starOwner, msg.sender, _tokenId);
+
+        payable(starOwner).transfer(starCost);
+
+        if (msg.value > starCost) {
+            payable(msg.sender).transfer(msg.value - starCost);
+        }
+
+        starsForSale[_tokenId] = 0;
+    }
+}
